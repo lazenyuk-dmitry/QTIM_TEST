2664e70701b75bc80573b0e97719136c57dfb6a7
Dmitry Lazenyuk <lazenyukdima@gmail.com>
2024-08-06 10:54:01 +0500

    Create main page.

---
+++ components/AppPostCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <NuxtLink class="post-card" to="/">
+    <img class="post-card__img" :src="data.image"/>
+    <span class="post-card__text">
+      {{ data.preview }}
+    </span>
+    <span class="post-card__more">
+      Read more
+    </span>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  data: {
+    createdAt: string,
+    description: string,
+    id: string,
+    image: string,
+    preview: string,
+    title: string,
+  },
+}
+
+const props = defineProps<Props>();
+const { data } = toRefs(props);
+</script>
+
+<style lang="scss" scoped>
+.post-card {
+  display: block;
+  cursor: pointer;
+
+  color: inherit;
+  text-decoration: none;
+  transition: .25s ease-in-out;
+
+  &:hover {
+    transform: translate(0, -30px);
+
+    .post-card__more {
+      display: block;
+    }
+  }
+}
+
+.post-card__img {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.post-card__text {
+  display: -webkit-box;
+  margin: 24px 0 0 0;
+  width:100%;
+  overflow: hidden;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+}
+
+.post-card__more {
+  display: none;
+  padding: 12px 0 0 0;
+  color: #E2BEFF;
+  position: absolute;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-
-  </div>
+  <NuxtLayout title="Articles">
+    <div class="article__list">
+      <div v-for="post in posts" class="article__card-wrap" :key="post.id">
+        <AppPostCard class="article__card" :data="post" />
+      </div>
+    </div>
+  </NuxtLayout>
 </template>
 
 <script setup>
-
+const { data: posts } = await useFetch('https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/');
 </script>
 
 <style lang="scss" scoped>
+.article__list {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 -15px;
+  gap: 40px 0;
+}
 
+.article__card-wrap {
+  flex: 0 1 25%;
+  padding: 0 15px;
+}
 </style>
